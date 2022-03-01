64b3c1cfa6a23e840fb17c81ca3ae0c73eac8054
“chirag-ghosh” <cghosh828049@gmail.com>
2022-03-01 16:38:37 +0530

    Created new pages;

---
+++ pages/list.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const List: NextPage = () => {
+  return (
+    <div className='blog-list'>
+      Here is the list of blogs
+    </div>
+  )
+}
+
+export default List
---
+++ pages/new.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const New: NextPage = () => {
+  return (
+    <div className='new-blog'>
+      Create new blog here.
+    </div>
+  )
+}
+
+export default New

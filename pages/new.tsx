48e1468d86e7e05987c446fc3bdf53db3d38a0c9
“chirag-ghosh” <cghosh828049@gmail.com>
2022-03-02 13:49:18 +0530

    Added blog create page;

--- pages/new.tsx
+++ pages/new.tsx
@@ -1,9 +1,39 @@
+import axios from 'axios';
 import type { NextPage } from 'next'
+import { useState } from 'react';
+import Editor from "rich-markdown-editor";
+import { Blog } from '../types/blog';
 
 const New: NextPage = () => {
+
+  const [title, setTitle] = useState<string>("");
+  const [markdownContent, setMarkdownContent] = useState<(() => string) | any>(() => "");
+
+  const submitBlog = () => {
+
+    const newBlog: Blog = {
+      title: title,
+      content: markdownContent,
+      publishDate: new Date()
+    }
+    
+    axios.post("http://localhost:3000/api/new", newBlog)
+          .then((res) => console.log(res))
+          .catch((err) => console.log(err))
+  }
+
   return (
     <div className='new-blog'>
-      Create new blog here.
+      <div className='title'>Create New <span>Blog</span></div>
+      <div className='top-bar'>
+      <input type='text' value={title} onChange={(event) => setTitle(event.target.value)} placeholder='Enter a title for the blog'></input>
+        <button className="button" onClick={() => submitBlog()}>Submit</button>
+      </div>
+      <div className='editor'>
+        <Editor 
+          onChange={(changeFunc) => setMarkdownContent(changeFunc)}
+        />
+      </div>
     </div>
   )
 }
